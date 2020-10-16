26b417b9f5aebf7d2422f1d00866a711536d1887
Maryia Ruzava <mardynotokay@gmail.com>
2020-10-17 00:00:49 +0200

    k

---
+++ tut10/task10/DTOs/Requests/AddStudentRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task10.DTOs.Requests
+{
+    public class AddStudentRequest
+    {
+        [Required(ErrorMessage = "You have to provide index number")]
+        public string IndexNumber { get; set; }
+
+        [Required(ErrorMessage = "You have to provide first name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "You have to provide last name")]
+
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "You have to provide birthdate")]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "You have to provide name of study")]
+        public string Studies { get; set; }
+
+        [Required(ErrorMessage = "You have to provide semester")]
+        public int Semester { get; set; }
+    }
+}
---
+++ tut10/task10/DTOs/Responses/AddStudentResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task10.DTOs.Responses
+{
+    public class AddStudentResponse
+    {
+        public int Semester { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime StartDate{ get; set; }
+    }
+}
