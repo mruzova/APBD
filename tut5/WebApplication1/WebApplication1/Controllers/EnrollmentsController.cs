b53144428bec2236c276be3ef8b758c59debe582
Maryia Ruzava <mardynotokay@gmail.com>
2020-10-16 22:53:44 +0200

    k

---
+++ tut5/WebApplication1/WebApplication1/Controllers/EnrollmentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs.Requests;
+using WebApplication1.DTOs.Responses;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/enrollment")]
+    [ApiController] //implicit model validation
+    public class EnrollmentsController : ControllerBase
+    {
+        //Tight coupling of classes
+        private IStudentServiceDb _service;
+
+        public EnrollmentsController(IStudentServiceDb service)
+        {
+            _service = service;
+        }
+
+        [HttpPost(Name = "EnrollStudent")]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            try
+            {
+                var response = _service.EnrollStudent(request);
+                return Created("EnrollStudent", response);
+            }
+            catch (InvalidOperationException)
+            {
+                return BadRequest("there is no such study");
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest("there is already a student with such index number");
+            }
+
+        }
+
+        [HttpPost("promote")]
+        public IActionResult PromoteStudents(PromoteStudentRequest request)
+        {
+
+            try
+            {
+                var response = _service.PromoteStudents(request);
+                return Created("PromoteStudents", response);
+            }
+            catch (SqlException)
+            {
+                return BadRequest("there are no students to promote or no such study");
+            }
+
+          
+        }
+
+    }
+
+}
---
+++ tut5/WebApplication1/WebApplication1/DTOs/Responses/EnrollStudentResponse.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DTOs.Responses
+{
+    public class EnrollStudentResponse 
+    {
+        public int Semester { get; set; }
+        public string LastName { get; set; }
+    }
+}
